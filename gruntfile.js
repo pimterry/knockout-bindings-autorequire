d071aa16edd82af4f1862f2dbec2d4fe9bf07e08
pimterry <pimterry@gmail.com>
2013-10-05 17:29:59 +0100

    Initial setup

---
+++ gruntfile.js
@@ -0,0 +1,91 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        // Task configuration.
+        jasmine: {
+            src: 'src/**/*.js',
+            options: {
+                host: "http://localhost:8000/",
+                specs: 'test/*-test.js',
+                template: require('grunt-template-jasmine-requirejs'),
+                templateOptions: {
+                    requireConfig: {
+                        baseUrl: "src",
+                        paths: {
+                            "test": "../test"
+                        }
+                    }
+                }
+            }
+        },
+        open: {
+            jasmine: {
+                path: 'http://127.0.0.1:8000/_SpecRunner.html'
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    keepalive: true
+                }
+            },
+            briefly: {
+                options: {
+                    port: 8000,
+                    keepalive: false
+                }
+            }
+        },
+        jshint: {
+            js: {
+                options: {
+                    jshintrc: 'src/.jshintrc'
+                },
+                src: ['src/**/*.js', '!src/lib/*.js']
+            },
+            test: {
+                options: {
+                    jshintrc: 'test/.jshintrc'
+                },
+                src: ['test/**/*.js', '!test/lib/*.js']
+            }
+        },
+        watch: {
+            js: {
+                files: '<%= jshint.js.src %>',
+                tasks: ['jshint:js', 'connect:briefly', 'jasmine']
+            },
+            test: {
+                files: '<%= jshint.test.src %>',
+                tasks: ['jshint:test', 'connect:briefly', 'jasmine']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-open');
+
+    // Default task.
+    grunt.registerTask('default', ['test']);
+    grunt.registerTask('test', ['jshint', 'connect:briefly', 'jasmine']);
+
+    // Test with a live server and an actual browser
+    grunt.registerTask('browsertest', ['jasmine:src:build', 'connect', 'open:jasmine']);
+
+};
---
+++ test/basicusage-test.js
@@ -0,0 +1,7 @@
+require(["knockout-bindings-autorequire"], function () {
+    describe("Knockout-bindings-autorequire", function () {
+        it("correctly loads a new binding that's used", function () {
+
+        });
+    });
+});

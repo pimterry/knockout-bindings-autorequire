ae09b2e918aa9b910256d0705444eebcbc59b565
pimterry <pimterry@gmail.com>
2013-10-05 18:06:53 +0100

    Fixed missing 'use strict' for jslint

--- test/basicusage-test.js
+++ test/basicusage-test.js
@@ -1,4 +1,6 @@
 require(["knockout-bindings-autorequire"], function () {
+    "use strict";
+
     describe("Knockout-bindings-autorequire", function () {
         it("correctly loads a new binding that's used", function () {
 
